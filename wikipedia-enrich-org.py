a2790f7859e555d26d60f4a167fb11f59b03e033
Aaron Williams <aaron@flex.io>
2020-06-10 10:52:56 -0500

    updated status codes used to trigger request retry

--- wikipedia-enrich-org.py
+++ wikipedia-enrich-org.py
@@ -247,7 +247,7 @@ def get_claim_info(object, item_id, language):
 def requests_retry_session(
     retries=3,
     backoff_factor=0.3,
-    status_forcelist=(500, 502, 504),
+    status_forcelist=(429, 500, 502, 503, 504),
     session=None,
 ):
     session = session or requests.Session()
